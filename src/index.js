5d1b8f9c818310e31f7663e65e0e1e803d56051b
Junior Medina <juniiormediina@gmail.com>
2021-03-31 09:55:01 -0500

    modified

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+/* Styles */
 import "./assets/styles/tailwind.css";
+
+/* Icons font-awesome */
 import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
